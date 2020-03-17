83da822a823acbda5438e805eee4f1d43784ecec
linqueta <lincolnrodrs@gmail.com>
2020-03-17 18:25:44 -0300

    improve i18n messages

---
+++ Coronav_rus_Brasil/lambda/us-east-1_corona-br-function/internationalization.js
@@ -0,0 +1,15 @@
+const skillNameSpeakable = 'Corona vírus Brasil'
+const options = 'Você pode perguntar a quantidade de casos que aconteceram no o Brasil ou em um estado'
+const fallback = `Hmmm, não sei nada sobre isso, ${options}`
+
+module.exports = {
+  'pt-BR': {
+    translation: {
+      WELCOME_MESSAGE: `Bem vindo ao ${skillNameSpeakable}, aqui ${options}`,
+      WELCOME_REPROMPT_MESSAGE: options,
+      HELP_MESSAGE: options,
+      FALLBACK_MESSAGE: fallback,
+      STOP_MESSAGE: 'Até mais'
+    }
+  }
+};
